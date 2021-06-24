668e5688b8a9f656e1acb367e5f0953a9dfd6b34
llf <llf@163.com>
2021-06-24 17:54:56 +0800

    struct

--- github.com/layery/method.go
+++ github.com/layery/method.go
@@ -6,11 +6,16 @@ import "fmt"
 
 结构体中的方法和接收者
 
+Golang中一切皆是类型, 所以会出现int, string类型也支持定义方法的魔性语法,
+这里可以类比PHP的一切皆是对象
+
 方法: 可以理解为PHP里的类方法, 是这个类所属的
+接收者: 可以理解为PHP的类本身($this 或 self)
 
 */
 
-// 声明一个Person类型的结构体
+// 声明一个Person类型的结构体,
+// 当一个变量的首字母是大写的, 意味着它是对外可见的
 type Person struct {
 	name string
 	age  int8
@@ -25,9 +30,27 @@ func newPerson(name string, age int8) *Person {
 }
 
 // 2. 为刚刚创建的Person类型的结构体, 定义一个方法
+//    方法里有两个传参, 第二个是接收者的类型, 第一个是接收者类型的首字母简写(约定成俗)
 func (p Person) Say() {
 	// fmt.Printf("%s %d", p.name+" 今年"+p.age+"岁了")
-	fmt.Printf("%v", p)
+	fmt.Printf("hello my name is %s \n", p.name)
+}
+
+func (p *Person) Hello(name string) {
+	fmt.Printf("hi I'm " + name + "\n")
+}
+
+/**
+方法的传参是指针类型的时候, 可以修改结构体内部的属性
+考虑一下3种情况下使用指针传参:
+	1. 需要修改接受者的值
+	2. 接受者是一个比较大的对象, 拷贝它会比较消耗资源, 但是拷贝指针代价就小很多
+	3. 保证一致性, 如果接收者的某一个方法使用了指针传参, 则为了保证一致性, 其他的方法也要使用指针传参
+*/
+func (p *Person) SetName(name string) *Person {
+	p.name = name
+	fmt.Println("now person's name is " + p.name)
+	return p
 }
 
 func main() {
@@ -36,7 +59,19 @@ func main() {
 	// 相当于PHP的new Object("llf", 30)
 
 	man := newPerson("llf", 30)
-	fmt.Printf("man 的类型是一个指针 %T", man)
-	// 接下来, 调用我们的say方法
+	fmt.Printf("man 的类型是一个指针 %T \n", man)
+	// 接下来, 调用我们的say方法, 由于构造函数返回的是一个指针
+	// 所以我们需要先取到指针的值, 再调用say方法
+	(*man).Say()
 
+	// golang里, 也可以不用显示的写出构造函数的值, 而直接用指针调用方法,
+	man.Say()
+
+	man.Hello("aaaa")
+
+	man.SetName("weidingyi")
+
+	man.Say()
+
+	fmt.Printf("现在man的类型是%T, man的值是%v \n", man, man)
 }
---
+++ github.com/layery/struct_2.go
@@ -0,0 +1,76 @@
+package main
+
+import "fmt"
+
+/**
+
+匿名结构体  &&  结构体的匿名字段
+
+*/
+
+// 1. 结构体的匿名字段, 即: 只有类型声明, 没有值, 那么如何访问结构体的字段呢?
+//    答案是访问它的类型即是访问了字段
+//    结构体中的字段具有唯一性, 不可重复
+type Human struct {
+	string
+	int
+}
+
+/**
+
+2. 嵌套结构体, 考虑如下两个结构体, Person中有city字段, 同时Address中也有city字段
+
+*/
+type Address struct {
+	province string
+	city     string
+	area     string
+}
+
+type Info struct {
+	city string
+}
+
+type Person struct {
+	name    string
+	age     int8
+	sex     int8
+	Address // 在这里, 我将Address结构体嵌套到了Person里
+	Info
+}
+
+func main() {
+	person1 := new(Human)
+
+	person2 := Human{
+		"weidingyi",
+		30,
+	}
+
+	fmt.Println(person1.int, person2.string, "\n\n")
+
+	// 使用嵌套结构体
+	person3 := Person{
+		name: "layery",
+		age:  30,
+		sex:  1,
+		Address: Address{
+			province: "北京-zi",
+			city:     "北京-zi",
+			area:     "朝阳区-zi",
+		},
+	}
+
+	fmt.Printf("%#v\n", person3)
+
+	fmt.Printf("可以通过.的方式来访问结构体的内部字段 %#v\n", person3.Address.province)
+
+	// 也可以直接访问结构内部的子结构体的字段
+	fmt.Printf("也可以直接访问子结构体的内部字段%#v\n", person3.Address.city)
+
+	// 当父结构体嵌套了多个结构体时, 如果子结构体中有重名的字段, 则访问的时候, 必须指定访问的是哪个子结构体
+	fmt.Printf("访问了Address下的city: %#v\n", person3.Address.city)
+	fmt.Printf("访问了Info下的city: %#v\n", person3.Info.city)
+
+	fmt.Printf("\n\n\n%#v\n", person3)
+}
