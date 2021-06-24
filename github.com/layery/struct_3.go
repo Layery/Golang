5428eb04960a434d9e7bf4927666345299f10544
llf <llf@163.com>
2021-06-24 18:09:34 +0800

    Golang的继承

---
+++ github.com/layery/struct_3.go
@@ -0,0 +1,23 @@
+package main
+
+/**
+
+Golang里的继承的实现方式
+
+*/
+
+type Animal struct {
+	name string // 动物都有名字
+	feet int8   // 动物都有腿
+	run  Func   // 动物都会跑
+}
+
+
+func p (...) {
+	fmt.Printf("%#v\n", ...)
+}
+
+func main() {
+	dog := new(Animal)
+
+}
